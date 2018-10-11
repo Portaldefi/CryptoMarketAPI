a73379f68ba9f19751ad58ac03439b1ac73d0408
Manoj Duggirala <manojdv@gmail.com>
2018-10-11 12:22:33 -0700

    fixed eth API

---
+++ cron/minutely.js
@@ -0,0 +1,72 @@
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection
+    .on('connected', () => {
+    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+})
+.on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+});
+
+global.fetch = require('node-fetch');
+const cc = require('cryptocompare');
+var asyncLoop = require('node-async-loop');
+
+var Minutely = require('../models/Minutely');
+var Coin = require('../models/Coin');
+
+Coin.find({}, function(err, coins) {
+  if (err) throw err;
+        asyncLoop(coins, function (item, next)
+        {
+            var fsym = item.symbol;
+            findandUpdate(fsym, function() {
+                 console.log('added '+fsym);
+                 next();
+            });
+        }, function (err)
+        {
+            if (err)
+            {
+                console.error('Error: ' + err.message);
+                return;
+            }
+            console.log('Finished!');
+        });
+
+});
+
+function findandUpdate(fsym, callback){
+        cc.histoMinute(fsym,'USD',{limit:2})
+        .then(data => {
+            
+            asyncLoop(data, function (item, next)
+            {
+                Minutely.update(
+                    {fsym:fsym},
+                    { '$addToSet': { price: item } },
+                        function(err, model) {
+                            console.log(err);
+                            next();
+                        }
+                    );
+            }, function (err)
+            {
+                if (err)
+                {
+                    console.error('Error: ' + err.message);
+                    return;
+                }
+                console.log('Finished!');
+                callback();
+            });
+
+        }).catch((error) => {
+            console.log(error);
+            callback();
+        });     
+}
