654fd7cf474cdbcb21e24db47504d3f647393cb9
Manoj Duggirala <manoj@tides.network>
2019-01-21 13:32:49 -0800

    added a try catch for websockets

--- ws.js
+++ ws.js
@@ -4,36 +4,44 @@ module.exports = function(io){
 
     io.on("connection", (client)=>{
         console.log("Connection from client: " + client.handshake.address);
+        console.log(client.handshake.query);
+        var exchange = "";
+        var type = "";
+        var symbol = "";
 
-        var exchange = client.handshake.query.ex;
-        var type = client.handshake.query.type;
-        var symbol = client.handshake.query.symbol;
+        exchange = client.handshake.query.ex;
+        type = client.handshake.query.type;
+        symbol = client.handshake.query.symbol;
 
         const exchng = new ccxws.binance();
 
-        const exchange_ccxt = new ccxt[type] ();
-        let symbolId = exchange_ccxt.market(symbol); 
-
-        if (type=="gdax"){
-            exchng = new ccxws.coinbasepro();
-        } else {
-            exchng = new ccxws[exchange]();
-        }
-
-        if (type=="depth"){
-            exchng.on("ticker", trade => client.emit(trade));
-            exchng.subscribeTicker(symbolId);
-        } else if (type=="ticker"){
-            exchng.on("l2snapshot", trade => client.emit(trade));
-            exchng.on("l2update", trade => client.emit(trade));
-            exchng.on("l3snapshot", trade => client.emit(trade));
-            exchng.on("l3update", trade => client.emit(trade));
-            exchng.subscribeLevel2Snapshots(symbolId);
-            exchng.subscribeLevel2Updates(symbolId);
-            exchng.subscribeLevel3Snapshots(symbolId);
-            exchng.subscribeLevel3Updates(symbolId);
-        } else {
-            return
+        try{
+            const exchange_ccxt = new ccxt[exchange] ();
+            let symbolId = exchange_ccxt.market(symbol); 
+    
+            if (type=="gdax"){
+                exchng = new ccxws.coinbasepro();
+            } else {
+                exchng = new ccxws[exchange]();
+            }
+    
+            if (type=="depth"){
+                exchng.on("ticker", trade => client.emit(trade));
+                exchng.subscribeTicker(symbolId);
+            } else if (type=="ticker"){
+                exchng.on("l2snapshot", trade => client.emit(trade));
+                exchng.on("l2update", trade => client.emit(trade));
+                exchng.on("l3snapshot", trade => client.emit(trade));
+                exchng.on("l3update", trade => client.emit(trade));
+                exchng.subscribeLevel2Snapshots(symbolId);
+                exchng.subscribeLevel2Updates(symbolId);
+                exchng.subscribeLevel3Snapshots(symbolId);
+                exchng.subscribeLevel3Updates(symbolId);
+            } else {
+                return
+            }
+        } catch(error){
+            console.log(error)
         }
 
         client.on('disconnect', () => console.log('Client disconnected ' + client.handshake.address));
