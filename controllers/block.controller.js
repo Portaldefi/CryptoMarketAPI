27830893d118c53f4867ecfd0e85e0200003d51c
Manoj Duggirala <manoj@tides.network>
2019-05-19 15:07:06 -0700

    added multiple address support

--- controllers/block.controller.js
+++ controllers/block.controller.js
@@ -46,6 +46,56 @@ exports.submit_tx = (req, res) => {
     }
 };
 
+// exports.address = (req, res) => {
+//     res.setHeader('Content-Type', 'application/json');
+//     if(!req.query) {
+//         return res.status(400).send({
+//             message: "Parameters can not be empty"
+//         });
+//     } else {
+//         var chain = req.query.chain;
+//         var coin = req.query.coin; 
+//         var add = req.query.address; 
+
+//         var url = "https://insight.bitpay.com/api"
+//         if (coin == "btc"){
+//             if (chain =="test"){
+//                 url = "https://test-insight.bitpay.com/api"
+//             } else {
+//                 url = "https://insight.bitpay.com/api"
+//             }
+//         } else if (coin == "bch"){
+//             if (chain == "test"){
+//                 url = "https://test-bch-insight.bitpay.com/api"
+//             } else {
+//                 url = "https://bch-insight.bitpay.com/api"   
+//             }
+//         }
+//         axios.get(url+'/txs/?address='+add)
+//         .then(function (response) {
+//           var txs = response.data
+//           axios.get(url+'/addr/'+add+'?noTxList=1')
+//           .then(function (response) {
+//             var balance = response.data   
+//             res.status(200).json({balance:balance, txs:txs})
+//           })
+//           .catch(function (error) {
+//             res.status(500).json(error.data)
+//           }); 
+//         })
+//         .catch(function (error) {
+//             axios.get(url+'/addr/'+add+'?noTxList=1')
+//             .then(function (response) {
+//               var balance = response.data   
+//               res.status(200).json({balance:balance, txs:txs})
+//             })
+//             .catch(function (error) {
+//               res.status(500).json(error.data)
+//             });
+//         }); 
+//     }
+// };
+
 exports.address = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     if(!req.query) {
@@ -55,44 +105,22 @@ exports.address = (req, res) => {
     } else {
         var chain = req.query.chain;
         var coin = req.query.coin; 
-        var add = req.query.address; 
-
-        var url = "https://insight.bitpay.com/api"
-        if (coin == "btc"){
-            if (chain =="test"){
-                url = "https://test-insight.bitpay.com/api"
-            } else {
-                url = "https://insight.bitpay.com/api"
-            }
-        } else if (coin == "bch"){
-            if (chain == "test"){
-                url = "https://test-bch-insight.bitpay.com/api"
-            } else {
-                url = "https://bch-insight.bitpay.com/api"   
-            }
-        }
-        axios.get(url+'/txs/?address='+add)
+        var add = req.query.address;
+        let tx_url = txParser(add, chain, coin);
+        let bl_url = balanceParser(add, chain, coin);
+        
+        axios.get(tx_url)
         .then(function (response) {
-          var txs = response.data
-          axios.get(url+'/addr/'+add+'?noTxList=1')
+          let txs =  {txs:response.data.items};
+          axios.get(bl_url)
           .then(function (response) {
-            var balance = response.data   
-            res.status(200).json({balance:balance, txs:txs})
+            let balArr = balParser(response.data);
+            res.status(200).json({balance:balArr, txs:txs})
           })
-          .catch(function (error) {
+        }).catch(function (error) {
             res.status(500).json(error.data)
-          }); 
-        })
-        .catch(function (error) {
-            axios.get(url+'/addr/'+add+'?noTxList=1')
-            .then(function (response) {
-              var balance = response.data   
-              res.status(200).json({balance:balance, txs:txs})
-            })
-            .catch(function (error) {
-              res.status(500).json(error.data)
-            });
-        }); 
+        });
+
     }
 };
 
@@ -105,7 +133,7 @@ exports.utxo = (req, res) => {
     } else {
         var chain = req.query.chain;
         var coin = req.query.coin; 
-        var add = req.query.address; 
+        var add = req.query.address.split(",");
 
         var url = "https://insight.bitpay.com/api"
         if (coin == "btc"){
@@ -122,7 +150,7 @@ exports.utxo = (req, res) => {
             }
         }
 
-        axios.get(url+'/addr/'+add+'/utxo')
+        axios.get(url+'/addrs/'+add+'/utxo')
         .then(function (response) {
             res.status(200).json(response.data)
         })
@@ -148,3 +176,50 @@ exports.valid = (req, res) => {
         res.send(valid);
     }
 };
+
+function balanceParser(address, chain, coin){
+    var url = "https://chain.api.btc.com/v3/address/"
+    if (coin == "btc"){
+       url = "https://chain.api.btc.com/v3/address/"
+    } else if (coin == "bch"){
+        url = "https://bch-chain.api.btc.com/v3/address/"
+    }
+    return url+address
+}
+
+function txParser(address, chain, coin){
+    var url = "https://insight.bitpay.com/api"
+    if (coin == "btc"){
+        if (chain =="test"){
+            url = "https://test-insight.bitpay.com/api"
+        } else {
+            url = "https://insight.bitpay.com/api"
+        }
+    } else if (coin == "bch"){
+        if (chain == "test"){
+            url = "https://test-bch-insight.bitpay.com/api"
+        } else {
+            url = "https://bch-insight.bitpay.com/api"   
+        }
+    }
+    return url+'/addrs/'+address+'/txs'
+}
+
+function balParser(json){
+    var balance,totalReceived,totalSent,
+    unconfirmedBalance,unconfirmedReceived,unconfirmedSent=0.0;
+    var data = json.data;
+    for (var i=0;i<data.length;i++){
+        var address = data[i];
+        totalReceived =+ address.received;
+        totalSent =+ address.sent;
+        unconfirmedReceived =+ address.unconfirmed_received;
+        unconfirmedSent =+ address.unconfirmed_sent;
+        balance =+ address.balance;
+    }
+    unconfirmedBalance = unconfirmedReceived-unconfirmedSent;
+    return {data:data,totalReceived:totalReceived,balance:balance,
+            totalSent:totalSent,unconfirmedBalance:unconfirmedBalance, unconfirmedReceived:unconfirmedReceived,
+            unconfirmedSent:unconfirmedSent
+        }
+}
